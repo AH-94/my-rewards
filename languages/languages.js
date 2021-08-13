dccacf34b3b8b55f0a0fb041efbe358d7b1c8aae
Alex <alexhaigh23@outlook.com>
2021-08-13 10:36:07 +0100

    completed my rewars code, ready for go-live

---
+++ languages/languages.js
@@ -0,0 +1,249 @@
+const languages  = [
+    {
+        id: "1619534768063",
+        englishText: "cash rewards!<span class=\"accent-text\">up to egp 600</span>",
+        arabicText: "المكافآت النقدية!<span class=\"accent-text translatable\">حتى 600 جنيه مصري</span>"
+    },
+    {
+        id: "1619534954039",
+        englishText: "Congratulations!",
+        arabicText: "تهانينا!"
+    },
+    {
+        id: "1619535216756",
+        englishText: "You’re one step closer to exciting cash rewards",
+        arabicText: "لقد اقتربت خطوة واحدة من الحصول على مكافآت نقدية مثيرة"
+    },
+    {
+        id: "1619535579479",
+        englishText: "To redeem your My Rewards Points and get cash, follow the instructions below.",
+        arabicText: "لاسترداد نقاط My Rewards الخاصة بك والحصول على النقود ، اتبع التعليمات أدناه."
+    },
+    {
+        id: "1619539315687",
+        englishText: "How to redeem",
+        arabicText: "كيفية تخليص"
+    },
+    {
+        id: "1619539441724",
+        englishText: "Contact the My Rewards Points Customer Service Centre on +2022 739 4050 / +202 2529 1040 between 9am to 6pm from Sunday to Thursday or through the live chat on https://myrewardspoints.com/en-eg",
+        arabicText: "اتصل بمركز خدمة عملاء My Rewards Points على +2022739 4050 / +202 2529 1040 بين الساعة 9 صباحًا حتى 6 مساءً من الأحد إلى الخميس أو من خلال الدردشة المباشرة على https://myrewardspoints.com/en-eg"
+    },
+    {
+        id: "1619539619670",
+        englishText: "Tell the agent how many My Rewards Points you wish to redeem. Please note that you can only redeem for cashback once during the campaign period.",
+        arabicText: "أخبر الوكيل بعدد نقاط مكافآتي التي ترغب في استبدالها. يرجى ملاحظة أنه يمكنك استرداد النقود مرة واحدة فقط خلال فترة الحملة."
+    },
+    /*
+    {
+        id: "1619539690543",
+        englishText: "Tell the agent the reward of your choice and redeem your My Rewards Points",
+        arabicText: "أخبر الوكيل بمكافأة من اختيارك واستبدل نقاط My Rewards الخاصة بك"
+    },
+    {
+        id: "1619539833082",
+        englishText: "Enjoy your reward and cashback offers!",
+        arabicText: "استمتع بمكافأتك وعروض استرداد النقود!"
+    },
+    */
+    {
+        id: "1619603191942",
+        englishText: "Terms & Conditions",
+        arabicText: "البنود و الظروف"
+    },
+    {
+        id: "1619603346911",
+        englishText: "The offer is available only to the members who received and opted in the<br> offer communication",
+        arabicText: "العرض متاح فقط للأعضاء الذين تلقوا واختاروا اتصال العرض"
+    },
+    {
+        id: "1619603427041",
+        englishText: "Redemption once made cannot be reverted",
+        arabicText: "الاسترداد مرة واحدة لا يمكن التراجع"
+    },
+    {
+        id: "1619603573324",
+        englishText: "Members can make only one redemption during the campaign period",
+        arabicText: "يمكن للأعضاء إجراء عملية استرداد واحدة فقط خلال فترة الحملة"
+    },
+    {
+        id: "1619603797642",
+        englishText: "The offer is applicable only once during the campaign period dates as<br> communicated in the offer email",
+        arabicText: "يسري العرض مرة واحدة فقط خلال تواريخ فترة الحملة كما هو موضح في البريد الإلكتروني للعرض"
+    },
+    {
+        id: "1619603882639",
+        englishText: "Redemptions for cash back can be made only by contacting MRP Contact centre<br> via Call or Chat",
+        arabicText: "يمكن إجراء عمليات استرداد النقود فقط عن طريق الاتصال بمركز اتصال MRP عبر الاتصال أو الدردشة"
+    },
+    {
+        id: "1621260480113",
+        englishText: "Cash will be credited to member's HSBC Credit Card in 7 working days post<br> the successful redemption by contacting MRP contact centre",
+        arabicText: "ستتم إضافة الأموال النقدية إلى بطاقة HSBC الائتمانية الخاصة بالعضو في غضون 7 أيام عمل بعد الاسترداد الناجح عن طريق الاتصال بمركز اتصال MRP"
+    },
+    {
+        id: "1621260498631",
+        englishText: "Members may contact HSBC contact centre directly to raise any concerns post<br> the successful redemption of points",
+        arabicText: "يمكن للأعضاء الاتصال بمركز اتصال HSBC مباشرةً للإبلاغ عن أي مخاوف بعد الاسترداد الناجح للنقاط"
+    },
+    {
+        id: "1621260521560",
+        englishText: "Rights to determine cash back value back solely lies with HSBC",
+        arabicText: "تعود حقوق تحديد قيمة الاسترداد النقدي إلى HSBC فقط"
+    },
+    {
+        id: "1621260545880",
+        englishText: "Cash back reward option is available only during the campaign period (dates<br> as communicated in offer email)",
+        arabicText: "يتوفر خيار مكافأة الاسترداد النقدي فقط خلال فترة الحملة (التواريخ كما هو موضح في البريد الإلكتروني للعرض)"
+    },
+    {
+        id: "1621260680520",
+        englishText: "My Rewards Programme has the rights to change offer T&Cs if deemed so",
+        arabicText: "برنامج My Rewards لديه الحق في تغيير شروط وأحكام العرض إذا اعتبر ذلك كذلك"
+    },
+    {
+        id: "1619624594267",
+        englishText: "Enjoy this offer on a range of exciting rewards including mobile, retail and travel.",
+        arabicText: "استمتع بهذا العرض على مجموعة من المكافآت المثيرة بما في ذلك الهاتف المحمول والبيع بالتجزئة والسفر."
+    },
+    {
+        id: "1619624870646",
+        englishText: "View all rewards",
+        arabicText: "عرض كل المكافآت"
+    },
+    {
+        id: "1619701571136",
+        englishText: "Home",
+        arabicText: "منزل، بيت"
+    },
+    {
+        id: "1619701686384",
+        englishText: "Rewards",
+        arabicText: "المكافآت"
+    },
+    {
+        id: "1619701840793",
+        englishText: "HSBC",
+        arabicText: "HSBC"
+    },
+    {
+        id: "1619701898647",
+        englishText: "General",
+        arabicText: "جنرال لواء"
+    },
+    {
+        id: "1619702243829",
+        englishText: "<img class=\"padlock\" src=\"./images/padlock.png\" alt=\"padlock icon\">Login",
+        arabicText: "<img class=\"padlock\" src=\"./images/padlock.png\" alt=\"padlock icon\">تسجيل الدخول"
+    },
+    {
+        id: "1619705825362",
+        englishText: "© 2021 Aimia ME FZ-LLC.",
+        arabicText: "© 2021 Aimia ME FZ-LLC."
+    },
+    {
+        id: "1619705919853",
+        englishText: "home",
+        arabicText: "الصفحة الرئيسية"
+    },
+    {
+        id: "1619706034233",
+        englishText: "Login",
+        arabicText: "تسجيل الدخول"
+    },
+    {
+        id: "1619706094236",
+        englishText: "Forgot Password",
+        arabicText: "هل نسيت كلمة السر"
+    },
+    {
+        id: "1619706183751",
+        englishText: "rewards",
+        arabicText: "المكافآت"
+    },
+    {
+        id: "1619706255843",
+        englishText: "Electronics & Homeware",
+        arabicText: "إلكترونيات وأدوات منزلية"
+    },
+    {
+        id: "1619706344897",
+        englishText: "Travel & Leisure",
+        arabicText: "السفر والترفيه"
+    },
+    {
+        id: "1619706395227",
+        englishText: "Trears & Gifts",
+        arabicText: "هدايا وهدايا"
+    },
+    {
+        id: "1619706472790",
+        englishText: "How to redeem",
+        arabicText: "كيفية تخليص"
+    },
+    {
+        id: "1619706609171",
+        englishText: "general",
+        arabicText: "جنرال لواء"
+    },
+    {
+        id: "1619706652217",
+        englishText: "Rewards Points Expiry",
+        arabicText: "انتهاء صلاحية نقاط المكافآت"
+    },
+    {
+        id: "1619706706442",
+        englishText: "Terms and Conditions",
+        arabicText: "الأحكام والشروط"
+    },
+    {
+        id: "1619706765311",
+        englishText: "FAQs",
+        arabicText: "أسئلة وأجوبة"
+    },
+    {
+        id: "1619706850748",
+        englishText: "Contact Us",
+        arabicText: "اتصل بنا"
+    },
+    {
+        id: "1619706910807",
+        englishText: "Your Privacy",
+        arabicText: "خصوصيتك"
+    },
+    {
+        id: "1620997679806",
+        englishText: "Redeem today!",
+        arabicText: "تخليص اليوم!"
+    },
+    {
+        id: "1620998081551",
+        englishText: "<strong>11,300</strong> points:",
+        arabicText: "<strong>11,300</strong> نقطة:"
+    },
+    {
+        id: "1620998392615",
+        englishText: "EGP 150",
+        arabicText: "150 جنيه مصري"
+    },
+    {
+        id: "1620998462491",
+        englishText: "<strong>22,500</strong> points:",
+        arabicText: "<strong> 22500 </strong> نقطة:"
+    },
+    {
+        id: "1620998660342",
+        englishText: "EGP 300",
+        arabicText: "300 جنيه مصري"
+    },
+    {
+        id: "1620998863591",
+        englishText: "<strong>45,000</strong> points:",
+        arabicText: "<strong> 45000 </strong> نقطة:"
+    },
+    {
+        id: "1620998926234",
+        englishText: "EGP 600",
+        arabicText: "600 جنيه مصري"
+    }
+]
